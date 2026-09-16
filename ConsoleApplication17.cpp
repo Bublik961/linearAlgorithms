2a623a41b8bbce2a483fa14f88c44b2ed44e03db
Bublik961 <ctoto096@gmail.com>
2026-09-16 12:49:51 +0700

    Add files via upload

---
+++ ConsoleApplication17.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double R3, R5, R8, R10;
+    double Rn;
+    const double a = 16.74, pi = 3.14, n = 3;
+    R3 = (a * sqrt(3)) / 3;
+    R5 = a * (sqrt(0.5 + (sqrt(5) / 10)));
+    R8 = a * (sqrt(1 + (sqrt(2) / 2)));
+    R10 = (a / 2) * (1 + sqrt(5));
+    Rn = a / (2 * sin(pi / n));
+    cout << "R3 = " << R3 << endl;
+    cout << "R5 = " << R5 << endl;
+    cout << "R8 = " << R8 << endl;
+    cout << "R10 = " << R10 << endl;
+    cout << "Check" << endl;
+    cout << "R3 = " << Rn << endl;
+
+
+    
+}
